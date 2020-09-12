a885039820e837474c218e15862f96eaef5da236
captproton <carl@wdwhub.net>
2020-09-11 18:55:36 -0700

    WARN: reset db before pushing up this change.

--- db/migrate/20200910030446_add_broadcaster_theme_reference_to_broadcaster_outline.rb
+++ db/migrate/20200910030446_add_broadcaster_theme_reference_to_broadcaster_outline.rb
@@ -1,5 +1,5 @@
 class AddBroadcasterThemeReferenceToBroadcasterOutline < ActiveRecord::Migration[6.0]
   def change
-    add_reference :broadcaster_outlines, :theme, null: false, foreign_key: true
+    add_reference :broadcaster_outlines, :broadcaster_theme, null: false, foreign_key: true
   end
 end
