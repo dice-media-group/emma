29cd7f3e8989055dfd86a3bc28a47108ff894e1c
captproton <carl@wdwhub.net>
2020-10-12 16:34:30 -0700

    CHANGE: updated social links

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,5 +2,8 @@ class BlogController < ApplicationController
 
   def index
     render :layout => 'frontdoor'
+    @facebook_url = PublisherAcct.find_by(name: "facebook").url
+    @twitter_url  = PublisherAcct.find_by(name: "twitter").url
+    @linkedin_url  = PublisherAcct.find_by(name: "linkedin").url
   end
 end
