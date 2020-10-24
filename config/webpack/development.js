b86cf81a9e4aadd2d025430a9c0c2cfbafeb392d
captproton <carl@wdwhub.net>
2020-10-24 10:31:28 -0700

    FIXED: WDS not serving up on correct port for ngrok

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -9,10 +9,17 @@ dotenv.config({ path: ".env", silent: true });
 
 const ngrokTunnel = process.env.ASSET_TUNNEL_URI;
 
+// Webpack Dev Server (WDS) is usually 3035.
+// Because we are accessing WDS through ngrok, everything is https.
+// HTTPS is normally port 443.
+// This also means that this config, w/o more work, connects through ngrok all the time.
+// Choose another port, and WDS will become disconnected.
+// This config seems to live in the browser during run time.
+//
 environment.config.merge({
   devServer: {
-    host: ngrokTunnel,
-    public: ngrokTunnel + ":3035",
+    port: "443",
+    public: ngrokTunnel,
   },
 });
 
