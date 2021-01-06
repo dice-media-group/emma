bf5447736b54872b00485ba1e06b5f6417c09367
captproton <carl@wdwhub.net>
2021-01-06 14:02:10 -0800

    ADD: backstage/home_info to edit setting

--- app/controllers/backstage/settings_controller.rb
+++ app/controllers/backstage/settings_controller.rb
@@ -2,6 +2,7 @@ class Backstage::SettingsController < ApplicationController
   def index
     @site = Site.first
     @biography                = @site.biography
+    @home_info                = @site.home_info
     @first_time               = @site.first_time
     @get_in_contact_content   = @site.get_in_contact_content
   end
