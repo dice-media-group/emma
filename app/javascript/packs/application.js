24bcc2d45c4afc67f4d9fcbe4b46fbda75c36723
captproton <carl@wdwhub.net>
2021-02-18 21:11:06 -0800

    CHANGE: Moved storage to Vuex to simplify.  Added ActionCable for sharing board.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,9 +14,31 @@ window.Rails = Rails;
 import "bootstrap";
 import "data-confirm-modal";
 import Vue from 'vue/dist/vue.esm'
+import Vuex from 'vuex'
 import App from '../app.vue'
 
-window.store = {}
+Vue.use(Vuex)
+
+window.store = new Vuex.Store({
+  state: {
+    lists: []
+  },
+
+  mutations: {
+    addList(state, data) {
+      state.lists.push(data)
+    },
+    addCard(state, data) {
+      const index = state.lists.findIndex(item => item.id == data.list_id)
+      state.lists[index].cards.push(data)
+    },
+    editCard(state, data) {
+      const list_index = state.lists.findIndex((item) => item.id == data.list_id)
+      const card_index = state.lists[list_index].cards.findIndex((item) => item.id == data.id)
+      state.lists[list_index].cards.splice(card_index, 1, data)
+    },
+  }
+})
 
 $(document).on("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip();
@@ -24,11 +46,12 @@ $(document).on("turbolinks:load", () => {
   // draggable lists
   var element = document.querySelector("#boards")
   if (element != undefined ) {
-    window.store.lists = JSON.parse(element.dataset.lists)
+    window.store.state.lists = JSON.parse(element.dataset.lists)
+
     const app = new Vue({
       el: element,
-      data: window.store,
-      template: "<App :original_lists='lists' />",
+      store: window.store,
+      template: "<App />",
       components: { App }
     })
   }
