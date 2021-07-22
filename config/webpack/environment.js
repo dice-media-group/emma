c8eea62395466d2b40abde4e5a3adfb464d53636
captproton <carl@wdwhub.net>
2021-07-22 16:47:28 -0700

    CHANGE: feature/update_webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,4 @@
-const { environment } = require("@rails/webpacker");
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-
-environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
-environment.loaders.prepend("vue", vue);
-const webpack = require("webpack");
-environment.plugins.append(
-  "Provide",
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-    Rails: "@rails/ujs",
-  })
-);
-
-module.exports = environment;
+// 
++environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
++environment.loaders.prepend('vue', vue)
++module.exports = environment
